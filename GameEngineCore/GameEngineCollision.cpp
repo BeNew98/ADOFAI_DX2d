5d0facf3610d3763687a8929eb34ce741653da3f
동찬 황 <shidge98@gmail.com>
2023-05-26 01:31:02 +0900

    업데이트

--- GameEngineCore/GameEngineCollision.cpp
+++ GameEngineCore/GameEngineCollision.cpp
@@ -2,16 +2,16 @@
 #include "GameEngineCollision.h"
 #include "GameEngineLevel.h"
 
-GameEngineCollision::GameEngineCollision()
+GameEngineCollision::GameEngineCollision() 
 {
 }
 
-GameEngineCollision::~GameEngineCollision()
+GameEngineCollision::~GameEngineCollision() 
 {
 }
 
 
-void GameEngineCollision::Start()
+void GameEngineCollision::Start() 
 {
 }
 
@@ -24,7 +24,7 @@ std::shared_ptr<GameEngineCollision> GameEngineCollision::Collision(int _TargetG
 
 	std::list<std::shared_ptr<GameEngineCollision>>& Group = GetLevel()->Collisions[_TargetGroup];
 
-	if (Type == ColType::MAX)
+	if (_ThisColType == ColType::MAX)
 	{
 		_ThisColType = Type;
 	}
@@ -50,7 +50,7 @@ std::shared_ptr<GameEngineCollision> GameEngineCollision::Collision(int _TargetG
 	return nullptr;
 }
 
-void GameEngineCollision::SetOrder(int _Order)
+void GameEngineCollision::SetOrder(int _Order) 
 {
 	std::shared_ptr<GameEngineCollision> ConThis = DynamicThis<GameEngineCollision>();
 
--- GameEngineCore/GameEngineCore_Resources.cpp
+++ GameEngineCore/GameEngineCore_Resources.cpp
@@ -248,12 +248,18 @@ void GameEngineCore::CoreResourcesInit()
 
 		std::vector<GameEngineFile> Files = NewDir.GetAllFile({ ".hlsl", ".fx" });
 
-		// 쉐이더 자동컴파일
-		GameEngineVertexShader::Load(Files[0].GetFullPath(), "Merge_VS");
-		GameEnginePixelShader::Load(Files[0].GetFullPath(), "Merge_PS");
+		// std::string FileString = Files[0].GetString();
 
-		GameEngineVertexShader::Load(Files[1].GetFullPath(), "Texture_VS");
-		GameEnginePixelShader::Load(Files[1].GetFullPath(), "Texture_PS");
+		for (size_t i = 0; i < Files.size(); i++)
+		{
+			GameEngineShader::AutoCompile(Files[i]);
+		}
+
+		//GameEngineVertexShader::Load(Files[0].GetFullPath(), "Merge_VS");
+		//GameEnginePixelShader::Load(Files[0].GetFullPath(), "Merge_PS");
+
+		//GameEngineVertexShader::Load(Files[1].GetFullPath(), "Texture_VS");
+		//GameEnginePixelShader::Load(Files[1].GetFullPath(), "Texture_PS");
 	}
 
 
