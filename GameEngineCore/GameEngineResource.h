0a528090f47e44a61096a6eba092e30604fe6968
동찬 황 <shidge98@gmail.com>
2023-06-21 02:08:26 +0900

    엔진 transform 수정

--- GameEngineCore/GameEngineResource.h
+++ GameEngineCore/GameEngineResource.h
@@ -26,19 +26,9 @@ public:
 	GameEngineResource& operator=(const GameEngineResource& _Other) = delete;
 	GameEngineResource& operator=(GameEngineResource&& _Other) noexcept = delete;
 
-	std::string_view GetPath() 
+	std::string_view GetPath()
 	{
 		return Path.c_str();
-	}
-
-	std::string_view GetName()
-	{
-		return Name.c_str();
-	}
-
-	std::string GetNameToString()
-	{
-		return Name;
 	}
 
 	void SetPath(const std::string_view& _Value)
@@ -46,12 +36,7 @@ public:
 		Path = _Value;
 	}
 
-	void SetName(const std::string_view& _Value)
-	{
-		Name = _Value;
-	}
-
-	static std::shared_ptr<ResourcesType> Find(const std::string_view& _Name) 
+	static std::shared_ptr<ResourcesType> Find(const std::string_view& _Name)
 	{
 		std::string UpperName = GameEngineString::ToUpper(_Name);
 
@@ -74,7 +59,7 @@ public:
 
 
 protected:
-	static std::shared_ptr<ResourcesType> CreateUnNamed() 
+	static std::shared_ptr<ResourcesType> CreateUnNamed()
 	{
 		std::shared_ptr<ResourcesType> NewRes = std::make_shared<ResourcesType>();
 		UnNamedRes.push_back(NewRes);
@@ -103,7 +88,6 @@ protected:
 
 private:
 	std::string Path;
-	std::string Name;
 
 	static std::map<std::string, std::shared_ptr<ResourcesType>> NamedResources;
 	static std::list<std::shared_ptr<ResourcesType>> UnNamedRes;
