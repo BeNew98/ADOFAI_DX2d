7fa065afb5cd10439761b6d56a0e7c77caeecaec
동찬 황 <shidge98@gmail.com>
2023-06-21 02:03:10 +0900

    사운드 기능 업데이트

--- GameEnginePlatform/GameEngineSound.h
+++ GameEnginePlatform/GameEngineSound.h
@@ -1,12 +1,62 @@
 #pragma once
 #include <GameEnginePlatform/ThirdParty/FMOD/inc/fmod.hpp>
 #include <string_view>
+#include <unordered_map> 
+
+class GameEngineSoundPlayer
+{
+private:
+	FMOD::Channel* Channel = nullptr;
+
+public:
+	GameEngineSoundPlayer()
+	{
+
+	}
+
+	GameEngineSoundPlayer(FMOD::Channel* _Channel)
+		: Channel(_Channel)
+	{
+
+	}
+
+	void Stop()
+	{
+		Channel->stop();
+	}
+	void SetPitch(float _Pitch)
+	{
+		Channel->setPitch(_Pitch);
+	}
+	void SetVolume(float _Volume)
+	{
+		Channel->setVolume(_Volume);
+	}
+	void SetLoopCount(float _Count)
+	{
+		Channel->setLoopCount(_Count);
+	}
+	void SetMute(bool _Value)
+	{
+		Channel->setMute(_Value);
+	}
+	void SetPause(bool _Value)
+	{
+		Channel->setPaused(_Value);
+	}
+};
 
 // 설명 :
 // 이녀석 자체가 사운드 파일 역할도 합니다.
 class GameEngineSound
 {
+private: // manager
+	static std::unordered_map<std::string, std::shared_ptr<GameEngineSound>> AllSound;
+
+
 public:
+	static void ResourcesClear();
+
 	static void SoundUpdate();
 
 	GameEngineSound();
@@ -18,12 +68,24 @@ public:
 	GameEngineSound& operator=(const GameEngineSound& _Other) = delete;
 	GameEngineSound& operator=(GameEngineSound&& _Other) noexcept = delete;
 
-	void SoundLoad(const std::string_view& _Path);
-	FMOD::Channel* Play();
+	static void Load(const std::string_view& _Path);
+
+	static void Load(const std::string_view& _Name, const std::string_view& _Path);
+
+	static GameEngineSoundPlayer Play(const std::string_view& _Name);
 
 protected:
 
 private:
+	void SoundLoad(const std::string_view& _Path);
+	FMOD::Channel* SoundPlay();
+	// 시퀸스
+	// 연관
+	// 어뎁터
+
+	// map 노드형 연관 컨테이너
+	// Hash기반 맵입니다. 
+
 	FMOD::Sound* FMODSound = nullptr;
 
 };
